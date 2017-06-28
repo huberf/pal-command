d5407b0810a5ec7e2625d624c5bbafc8b820b381
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-06-28 12:59:12 -0500

    Add support for MongoDB as the save source

--- index.js
+++ index.js
@@ -20,50 +20,98 @@ app.use(cookieParser());
 //Setting up session handling
 app.use(session({secret: 'p4ls4ysh1'}));
 
-var commandQueue = {};
+// Setup command queue and determine DB vs. in-memory var
+// a little messy, but gets the job done
+var DB_ACTIVE = true;
 
 var createUuid = () => {
   return uuid.v4();
 }
 
-var createCommand = (clientId, commandId, command) => {
-  if (commandQueue[clientId]) {
-    commandQueue[clientId].push({command, id: commandId, done: false});
-  } else {
-    commandQueue[clientIid] = [{command, id: commandId, done: false}];
-  }
-}
+if (!DB_ACTIVE) {
+  var commandQueue = {};
 
-var completeCommand = (clientId, commandId) => {
-  var success = true;
-  var index = -1;
-  var commands = [];
-  if (commandQueue[clientId]) {
-    commands = commandQueue[clientId];
-  } else {
-    commands = [];
-  }
-  var foundOne = false;
-  for (var i = 0; i < commands.length; i++) {
-    if (commands[i].id == commandId) {
-      foundOne = true;
-      console.log('Marking command as complete');
-      commandQueue[clientId][i].done = true;
+  var createCommand = (clientId, commandId, command) => {
+    if (commandQueue[clientId]) {
+      commandQueue[clientId].push({command, id: commandId, done: false, creation: Date.now()});
+    } else {
+      commandQueue[clientIid] = [{command, id: commandId, done: false, creation: Date.now()}];
     }
   }
-  var message = "None";
-  if (!foundOne) {
-    message = "Command specified could not be found";
-    success = false;
-  }
-  return {success, message}
-}
 
-var getCommands = (clientId) => {
-  if(!commandQueue[clientId]) {
-    commandQueue[clientId] = [];
+  var completeCommand = (clientId, commandId) => {
+    var success = true;
+    var index = -1;
+    var commands = [];
+    if (commandQueue[clientId]) {
+      commands = commandQueue[clientId];
+    } else {
+      commands = [];
+    }
+    var foundOne = false;
+    for (var i = 0; i < commands.length; i++) {
+      if (commands[i].id == commandId) {
+        foundOne = true;
+        console.log('Marking command as complete');
+        commandQueue[clientId][i].done = true;
+      }
+    }
+    var message = "None";
+    if (!foundOne) {
+      message = "Command specified could not be found";
+      success = false;
+    }
+    return {success, message}
   }
-  return commandQueue[clientId];
+
+  var getCommands = (clientId) => {
+    return new Promise((fulfill, reject) => {
+      if(!commandQueue[clientId]) {
+        commandQueue[clientId] = [];
+      }
+      fulfill(commandQueue[clientId]);
+    });
+  }
+} else {
+  // Initialze DB query var
+	var mongoose = require('mongoose');
+  var config = require('./config.json');
+  db = mongoose.connect(`mongodb://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`);
+  const commandSchema = new mongoose.Schema({
+    clientId: String,
+    command: mongoose.Schema.Types.Mixed,
+    id: String,
+    done: Boolean,
+    creation: Number,
+  });
+  const Command = mongoose.model('Command', commandSchema);
+  var createCommand = (clientId, commandId, command) => {
+    var newCommand = new Command({
+      command,
+      clientId,
+      id: commandId,
+      done: false,
+      creation: Date.now(),
+    });
+    newCommand.save();
+  }
+	var completeCommand = (clientId, commandId) => {
+    Command.findOne({id: commandId, clientId}, (err, command) => {
+      if (command) {
+        command.done = true;
+        command.save();
+      } else {
+        console.log('Error: No command found for supplied command ID and client ID');
+      }
+    });
+	}
+  var getCommands = (clientId) => {
+    return new Promise((fulfill, reject) => {
+      Command.find({clientId, done: false}, (err, commands) => {
+        fulfill(commands);
+      });
+    });
+  }
 }
 
 app.get('/', (req, res) => {
@@ -103,8 +151,10 @@ app.post('/add/:id', (req, res) => {
 
 // Endpoints for compatibility with devices that don't support websockets
 app.get('/retrieve/:id', (req, res) => {
-  var toSend = {name: 'pal-command', version: '0.1.0', commands: getCommands(req.params.id)};
-  res.send(toSend);
+  getCommands(req.params.id).then((commands) => {
+    var toSend = {name: 'pal-command', version: '0.1.0', commands};
+    res.send(toSend);
+  });
 });
 
 app.post('/complete/:clientId', (req, res) => {
@@ -125,8 +175,9 @@ io.sockets.on('connection', function(socket){
   socket.on('handshake', function (data) {
     console.log(data);
     socket.id = data.id;
-    var commands = getCommands(data.id);
-    socket.emit('handshake', {name: 'pal-command', version: '0.1.0', commands: commands.map((e) => {if (e.done == false) { return e;}})});
+    getCommands(data.id).then((commands) => {
+      socket.emit('handshake', {name: 'pal-command', version: '0.1.0', commands: commands.map((e) => {if (e.done == false) { return e;}})});
+    });
   });
   socket.on('complete', function(data) {
     completeCommand(data.clientId, data.id);
