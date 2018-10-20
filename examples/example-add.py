83679d78d59cec1f5074f9ef52ef16d58331be7d
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-10-19 20:19:50 -0400

    [examples] add group permission auth

--- examples/example-add.py
+++ examples/example-add.py
@@ -4,4 +4,11 @@ import json
 server = 'http://localhost:1999/'
 client = 'demo'
 
+group = 'test_group'
 r.post(server + 'add/' + client, json={'command': {'action': 'basic', 'text': 'Test success?'}})
+myId = 'main-client'
+r.post(server + 'group/permissions/' + group + '/' + myId, json={
+    'type': 'post',
+    'auth_key': 'k23j5l2h42'
+    });
+r.post(server + 'group/add/' + group + '/' + myId, json={'command': {'action': 'basic', 'text': 'Group message post'}})
