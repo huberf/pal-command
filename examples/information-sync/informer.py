d3aacd6e4a361faf84a038d4e8619ed7b1206214
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-10-23 07:25:45 -0400

    [examples] add data request and response example

---
+++ examples/information-sync/informer.py
@@ -0,0 +1,57 @@
+from socketIO_client import SocketIO, LoggingNamespace
+import time
+
+myId = 'informer'
+
+def execute(data):
+    global socketIO
+    success = True
+    if data['command']['type'] == 'datarequest':
+        if data['command']['data'] == 'system_time':
+            # Now post time
+            socketIO.emit('addtogroup', {
+                'id': myId,
+                'groupId': 'test_group',
+                'command': {
+                    'type': 'dataresponse',
+                    'data': 'system_time',
+                    'val': time.time()
+                    }
+                });
+    else:
+        #success = False
+        print('Can\'t process specified action.')
+        print(data)
+    if success:
+        socketIO.emit('complete', {'clientId': myId, 'id': data['id']})
+
+def on_handshake(data):
+   print(data)
+   for i in data['commands']:
+       if not i == None:
+           execute(i)
+
+def on_command(data):
+    print(data)
+    execute(data)
+
+socketIO = SocketIO('localhost', 1999, LoggingNamespace)
+socketIO.on('handshake', on_handshake)
+socketIO.on('command', on_command)
+
+# Listen
+socketIO.emit('handshake', {'id': myId})
+socketIO.wait(seconds=1)
+
+print("Authorizing with group");
+socketIO.emit('groupauth', {
+    'id': myId,
+    'group_name': 'test_group',
+    'auth_key': 'k23j5l2h42',
+    'type': 'all'
+    });
+socketIO.wait(seconds=1);
+
+# Listen only once
+while True:
+    socketIO.wait(seconds=1)
---
+++ examples/information-sync/requester.py
@@ -0,0 +1,63 @@
+from socketIO_client import SocketIO, LoggingNamespace
+import time
+
+myId = 'requester'
+
+def execute(data):
+    success = True
+    if data['command']['type'] == 'dataresponse':
+        print('System time is: ' + str(data['command']['val']))
+        # Now request time again
+        time.sleep(1) # Wait 1 second before new request
+        socketIO.emit('addtogroup', {
+            'id': myId,
+            'groupId': 'test_group',
+            'command': {
+                'type': 'datarequest',
+                'data': 'system_time'
+                }
+            });
+    else:
+        #success = False
+        print('Can\'t process specified action.')
+        print(data)
+    if success:
+        socketIO.emit('complete', {'clientId': myId, 'id': data['id']})
+
+def on_handshake(data):
+   print(data)
+   for i in data['commands']:
+       if not i == None:
+           execute(i)
+
+def on_command(data):
+    print(data)
+    execute(data)
+
+socketIO = SocketIO('localhost', 1999, LoggingNamespace)
+socketIO.on('handshake', on_handshake)
+socketIO.on('command', on_command)
+
+# Listen
+socketIO.emit('handshake', {'id': myId})
+socketIO.wait(seconds=1)
+
+print("Authorizing with group");
+socketIO.emit('groupauth', {
+    'id': myId,
+    'group_name': 'test_group',
+    'auth_key': 'k23j5l2h42',
+    'type': 'all' });
+socketIO.emit('addtogroup', {
+    'id': myId,
+    'groupId': 'test_group',
+    'command': {
+        'type': 'datarequest',
+        'data': 'system_time'
+        }
+    });
+socketIO.wait(seconds=1);
+
+# Listen only once
+while True:
+    socketIO.wait(seconds=1)
